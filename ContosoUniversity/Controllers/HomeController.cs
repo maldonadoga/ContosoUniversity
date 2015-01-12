6851e26fdfff32333579ea29d9af6398939bc644
maldonadoga <maldonadoga@hotmail.com>
2015-01-12 15:42:14 +0100

    Added about paged list statistics

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+using PagedList;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult About(int? page)
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            
+            data = data.OrderBy(e => e.EnrollmentDate);
 
-            return View();
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            return View(data.ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult Contact()
@@ -25,6 +40,12 @@ namespace ContosoUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
